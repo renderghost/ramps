82ac4e0058ebe908d60531f1bdf4df53ac0d7782
Barry Prendergast <barry.m.prendergast@gmail.com>
2023-07-26 13:26:08 +0200

    fixed bug with adding removing stops

--- ramps.js
+++ ramps.js
@@ -1,3 +1,51 @@
+function initialize() {
+    const stops = document.getElementById("stops").value;
+    createSVGStops(stops, true);
+    createColorPickers(stops, true);
+    attachColorChangeHandlers(stops);
+}
+
+function createSVGStops(stops, isInitialLoad) {
+    const gradientTypes = ['linear', 'radial'];
+    gradientTypes.forEach(gradientType => {
+        const gradient = document.getElementById(`${gradientType}-gradient`);
+        // Clear existing children
+        while (gradient.firstChild) {
+            gradient.firstChild.remove();
+        }
+        // Create stops
+        for (let i = 0; i < stops; i++) {
+            const stop = document.createElementNS('http://www.w3.org/2000/svg', 'stop');
+            stop.setAttribute('offset', `${(i / (stops - 1)) * 100}%`);
+            const color = localStorage.getItem(`color-${i}`) || '#000000';
+            stop.setAttribute('stop-color', color);
+            gradient.appendChild(stop);
+        }
+    });
+}
+
+function createColorPickers(stops, isInitialLoad) {
+    let stopElements = "";
+    for (let i = 0; i < stops; i++) {
+        let color;
+        if (isInitialLoad) {
+            color = getRandomColor();
+            localStorage.setItem(`color-${i}`, color);
+        } else {
+            color = localStorage.getItem(`color-${i}`) || '#000000';
+        }
+        stopElements += `<input type="color" id="color-${i}" value="${color}">`;
+    }
+    const colorPickerContainer = document.getElementById("color-picker");
+    colorPickerContainer.innerHTML = stopElements;
+}
+
+function attachColorChangeHandlers(stops) {
+    for (let i = 0; i < stops; i++) {
+        handleColorChange(i);
+    }
+}
+
 function handleSliderChange(id, attribute) {
     const element = document.getElementById(id);
     element.addEventListener("input", function () {
@@ -11,6 +59,7 @@ function handleColorChange(index) {
     element.addEventListener("input", function () {
         const gradientType = document.querySelector('input[name="gradient-type"]:checked').value;
         document.querySelector(`#${gradientType}-gradient stop:nth-child(${index + 1})`).setAttribute('stop-color', this.value);
+        localStorage.setItem(`color-${index}`, this.value); // store the color in local storage
     });
 }
 
@@ -18,29 +67,9 @@ function handleStopsChange() {
     const stopElement = document.getElementById("stops");
     stopElement.addEventListener("input", function () {
         const stops = this.value;
-        let stopElements = "";
-        for (let i = 0; i < stops; i++) {
-            stopElements += `<input type="color" id="color-${i}" value="#000000">`;
-        }
-        document.getElementById("color-picker").innerHTML = stopElements;
-
-        const gradientTypes = ['linear', 'radial'];
-        gradientTypes.forEach(gradientType => {
-            const gradient = document.getElementById(`${gradientType}-gradient`);
-            while (gradient.firstChild) {
-                gradient.firstChild.remove();
-            }
-            for (let i = 0; i < stops; i++) {
-                const stop = document.createElementNS('http://www.w3.org/2000/svg', 'stop');
-                stop.setAttribute('offset', `${(i / (stops - 1)) * 100}%`);
-                stop.setAttribute('stop-color', '#000000');
-                gradient.appendChild(stop);
-            }
-        });
-
-        for (let i = 0; i < stops; i++) {
-            handleColorChange(i);
-        }
+        createSVGStops(stops, false);
+        createColorPickers(stops, false);
+        attachColorChangeHandlers(stops);
     });
 }
 
@@ -78,9 +107,13 @@ function handleRandomise() {
         const stops = document.getElementById("stops").value;
         for (let i = 0; i < stops; i++) {
             const randomColor = getRandomColor();
+            // Update color picker
             document.getElementById(`color-${i}`).value = randomColor;
+            // Update gradient stop
             const gradientType = document.querySelector('input[name="gradient-type"]:checked').value;
             document.querySelector(`#${gradientType}-gradient stop:nth-child(${i + 1})`).setAttribute('stop-color', randomColor);
+            // Save color to local storage
+            localStorage.setItem(`color-${i}`, randomColor);
         }
     });
 }
@@ -94,3 +127,5 @@ handleStopsChange();
 handleSpreadChange();
 handleGradientTypeChange();
 handleRandomise();
+
+window.onload = initialize;
